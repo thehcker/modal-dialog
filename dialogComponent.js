798cd249149c186b470dfdf4878515bd8300f25f
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2021-11-27 10:26:50 +0300

    Add main.html and dialogCompoent

---
+++ dialogComponent.js
@@ -0,0 +1,35 @@
+class DialogComponent {
+  alertMessage(id) {
+    // let btn = document.getElementById(id);
+    // btn.addEventListener("click", () => {
+    //   console.log("Hello");
+    //   alert("Are you sure you want to continue?");
+    // });
+
+    var modal = document.getElementById("myModal");
+    // Get the button that opens the modal
+    var btn = document.getElementById("button");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+  }
+}
+let msg = new DialogComponent();
+msg.alertMessage("button");
